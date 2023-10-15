09ca66934c1dc420b2bf4346063d5e5c301418ea
asus <asus@ASUS>
2023-10-15 22:38:36 +0300

    Add project files.

---
+++ Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs6Prob2
+{
+    internal class Fibonacci
+    {
+        public static int FibonacciNumber(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return FibonacciNumber(n - 1) + FibonacciNumber(n - 2);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Curs6Prob2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti numarul: ");
+            bool canConvert = false;
+            while (!canConvert)
+            {
+                canConvert = Int32.TryParse(Console.ReadLine(), out int numar);
+                if (!canConvert)
+                {
+                    Console.WriteLine("Numarul nu este valid. Incercati din nou ");
+                }
+                else
+                {
+
+                    Console.WriteLine($"Primele {numar} numere din secventa Fibonacci sunt:");
+                    for (int i = 0; i < numar; i++)
+                    {
+                        int fibonacciNumber = Fibonacci.FibonacciNumber(i);
+                        Console.Write(fibonacciNumber + " ");
+                    }
+                }
+            }
+        }
+    }
+}
